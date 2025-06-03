34907e714f944bf174f272ddae23394a0060ddee
Suhaas Joshi <s-joshi@ti.com>
2025-06-03 14:00:48 +0530

    ti: config: boards: Add AM62L-EVM support
    AM62L [0] is a new low-cost, low-power SoC from Texas Instruments. It
    features 4x Cortex-A53 cores, 1x HDMI display, 1x DSI display, support
    for wifi using M2-CC33x1 cards, ADC, ethernet etc.
    
    AM62L-EVM is the evaluation board featuring the AM62L SoC, with
    circuitry and support for showcasing all of the above listed SoC
    features.
    
    Add support for this board in Armbian.
    
    In bootscripts/boot-k3.cmd, disable cursor on framebuffer. This will
    effect all boards, but it isn't required on any. The reason to do this
    is that the blinking cursor disturbs ti-lvgl-demo on AM62L.
    
    [0] https://www.ti.com/product/AM62L
    
    Signed-off-by: Suhaas Joshi <s-joshi@ti.com>

--- config/bootscripts/boot-k3.cmd
+++ config/bootscripts/boot-k3.cmd
@@ -5,3 +5,5 @@ name_rd=uInitrd
 get_rd_mmc=load mmc ${bootpart} ${rdaddr} ${bootdir}/${name_rd}
 
 uenvcmd=run get_rd_${boot}; env set rd_spec ${rdaddr}:${filesize}; setexpr fdtfile sub ti/ti ti; run bootcmd_ti_mmc
+
+optargs=vt.global_cursor_default=0
